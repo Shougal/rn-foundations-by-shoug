5d07dbe9f9d751449098c5b01cb0e4ddd784857f
shougal <xdq9qa@virginia.edu>
2025-07-03 23:25:02 +0300

    style(link): created initial style for link

---
+++ src/components/Link/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+import { fontSize } from "../../styles/theme";
+
+export const style = StyleSheet.create({
+  default: {
+    fontSize: fontSize.sm,
+    textDecorationLine: "underline",
+    textDecorationStyle: "solid",
+    color: "#0071DA",
+  },
+});
