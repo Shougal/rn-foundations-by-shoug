cc509914f6805667710fe7444f894d77301642d7
shougal <xdq9qa@virginia.edu>
2025-07-03 21:56:32 +0300

    Style(Note): adjusted style to call global themes

--- src/components/Note/style.ts
+++ src/components/Note/style.ts
@@ -1,11 +1,13 @@
 import { StyleSheet } from "react-native";
 
 import { VariantType } from "./type";
+import { fontSize } from "../../styles/theme";
 
 const style = StyleSheet.create({
   info: {
     flexShrink: 1, // Allows it to shrink if needed
     flexWrap: "wrap", // Ensures text wraps to the next line
+    fontSize: fontSize.sm,
   },
 });
 
