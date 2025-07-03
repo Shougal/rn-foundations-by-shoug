38caeec0cb7d0407a05603d7653e86fc35d813a0
shougal <xdq9qa@virginia.edu>
2025-07-03 21:56:01 +0300

    style(Section): adjusted style

--- src/components/Section/style.ts
+++ src/components/Section/style.ts
@@ -1,16 +1,20 @@
 import { StyleSheet } from "react-native";
 
+import { padding, cornerRadius, fontSize } from "../../styles/theme";
+
 export const style = StyleSheet.create({
   container: {
-    padding: 16,
+    padding: padding.m,
     backgroundColor: "#f0f0f0",
-    borderRadius: 12,
+    borderRadius: cornerRadius.m,
     flexShrink: 1,
   },
   header: {
     textAlign: "center",
     fontWeight: "bold",
-    fontSize: 18,
+    fontSize: fontSize.m,
+    color: "blue",
+    //TODO: font family
     marginBottom: 8,
   },
 });
