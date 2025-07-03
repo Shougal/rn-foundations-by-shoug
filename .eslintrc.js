0a1afeb9dc9ea81faf8992526359f216b23a0d18
shougal <xdq9qa@virginia.edu>
2025-07-03 14:30:04 +0300

    chore(eslint): configure ESLint with TypeScript, React, and naming rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,97 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'import',
+    'unused-imports',
+    '@typescript-eslint',
+    'filenames',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    // React & Hooks
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // Unused imports
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+
+    // Naming conventions
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variableLike',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'function',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'typeLike', // interfaces, types, classes, enums
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+      },
+      {
+        selector: 'property',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+    ],
+
+    // File naming (kebab-case only)
+    'filenames/match-regex': [2, '^[a-z0-9\\-]+$', true],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+  {
+    files: ['App.tsx', 'App.test.tsx'],
+    rules: {
+      'filenames/match-regex': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+    },
+  },
+],
 };
+
