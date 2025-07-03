635f51321f66d00e5102d6a1dd7475218a29ff07
shougal <xdq9qa@virginia.edu>
2025-07-03 15:41:00 +0300

    style(section): add base styles for component layout

---
+++ src/components/Section/style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 12,
+    flexShrink: 1,
+  },
+  header: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 8,
+  },
+});
