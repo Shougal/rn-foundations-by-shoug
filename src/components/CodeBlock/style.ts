b3b8caa811d9fa440f164e725c35d9e5e2797ffb
shougal <xdq9qa@virginia.edu>
2025-07-04 10:45:12 +0300

    style(code): created code initial style

---
+++ src/components/CodeBlock/style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const style = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: "#fff",
+  },
+  code: {
+    fontFamily: "Courier", // Use a monospace font
+    backgroundColor: "#f4f4f4",
+    padding: 12,
+    borderRadius: 6,
+    color: "#333",
+  },
+});
