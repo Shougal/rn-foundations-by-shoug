820ca5df967ce633acc0307a181033182a7fbbb5
shougal <xdq9qa@virginia.edu>
2025-07-04 12:07:34 +0300

    fix(List): Removed flatList as I can't insert inside a scrollable in pages and made plain list instead

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,25 +1,25 @@
-import { FlatList, Text } from "react-native";
+import { Text, View } from "react-native";
 
 import { ListProps } from "./type";
 import { style } from "./style";
 
-export const List = ({ variant, items }: ListProps) => {
+export const List = ({ variant, items, title }: ListProps) => {
   //P.S Avoid having key as index. Using the index as the key
   // can cause bugs in React's rendering when the list: Changes order ,
   // Adds or removes items, Has duplicate or unstable content
   // I chose to do it because this list usage is short and static.
   // It’s just rendering bullet points (not interactive items).
   return (
-    <FlatList
-      style={style.items}
-      data={items}
-      keyExtractor={(item, index) => `${item}-${index}`}
-      renderItem={({ item, index }) => (
-        <Text>
-          {variant === "ordered" ? `${index + 1}. ` : "\u2022 "}
-          {item}
-        </Text>
-      )}
-    />
+    <View style={style.container}>
+      {title && <Text style={style.title}>{title}</Text>}
+      {items.map((item, index) => (
+        <View key={index} style={style.itemContainer}>
+          <Text style={style.bullet}>
+            {variant === "ordered" ? `${index + 1}.` : "\u2022"}
+          </Text>
+          <Text style={style.itemText}>{item}</Text>
+        </View>
+      ))}
+    </View>
   );
 };
